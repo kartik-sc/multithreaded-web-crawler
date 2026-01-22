0e283260f38e0e1030c57c9f7f38c103a248ec17
kartik-sc <kartiksatishchandra@gmail.com>
2026-01-22 17:58:02 +0000

    intial commit - uploading all files

---
+++ CMakeFiles/crawler.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/kartik/wub/src/downloader.cpp" "CMakeFiles/crawler.dir/src/downloader.cpp.o" "gcc" "CMakeFiles/crawler.dir/src/downloader.cpp.o.d"
+  "/home/kartik/wub/src/main.cpp" "CMakeFiles/crawler.dir/src/main.cpp.o" "gcc" "CMakeFiles/crawler.dir/src/main.cpp.o.d"
+  "/home/kartik/wub/src/parser.cpp" "CMakeFiles/crawler.dir/src/parser.cpp.o" "gcc" "CMakeFiles/crawler.dir/src/parser.cpp.o.d"
+  "/home/kartik/wub/src/storage_manager.cpp" "CMakeFiles/crawler.dir/src/storage_manager.cpp.o" "gcc" "CMakeFiles/crawler.dir/src/storage_manager.cpp.o.d"
+  "/home/kartik/wub/src/thread_manager.cpp" "CMakeFiles/crawler.dir/src/thread_manager.cpp.o" "gcc" "CMakeFiles/crawler.dir/src/thread_manager.cpp.o.d"
+  "/home/kartik/wub/src/url_frontier.cpp" "CMakeFiles/crawler.dir/src/url_frontier.cpp.o" "gcc" "CMakeFiles/crawler.dir/src/url_frontier.cpp.o.d"
+  "/home/kartik/wub/src/utils.cpp" "CMakeFiles/crawler.dir/src/utils.cpp.o" "gcc" "CMakeFiles/crawler.dir/src/utils.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
