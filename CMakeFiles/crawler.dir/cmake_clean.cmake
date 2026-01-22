3f27996bc888cb78efae1fc48373064fb152f56f
kartik-sc <kartiksatishchandra@gmail.com>
2026-01-22 18:09:57 +0000

    declutter commit

---
+++ CMakeFiles/crawler.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/crawler.dir/src/downloader.cpp.o"
+  "CMakeFiles/crawler.dir/src/downloader.cpp.o.d"
+  "CMakeFiles/crawler.dir/src/main.cpp.o"
+  "CMakeFiles/crawler.dir/src/main.cpp.o.d"
+  "CMakeFiles/crawler.dir/src/parser.cpp.o"
+  "CMakeFiles/crawler.dir/src/parser.cpp.o.d"
+  "CMakeFiles/crawler.dir/src/storage_manager.cpp.o"
+  "CMakeFiles/crawler.dir/src/storage_manager.cpp.o.d"
+  "CMakeFiles/crawler.dir/src/thread_manager.cpp.o"
+  "CMakeFiles/crawler.dir/src/thread_manager.cpp.o.d"
+  "CMakeFiles/crawler.dir/src/url_frontier.cpp.o"
+  "CMakeFiles/crawler.dir/src/url_frontier.cpp.o.d"
+  "CMakeFiles/crawler.dir/src/utils.cpp.o"
+  "CMakeFiles/crawler.dir/src/utils.cpp.o.d"
+  "crawler"
+  "crawler.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/crawler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
